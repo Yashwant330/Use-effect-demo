fada1be7fd7cb4ad0a6c0c73c6d7ae21271a2746
Yashwant330 <yashwanthalwai161@gmail.com>
2026-03-31 11:48:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+const App = () => {
+
+  let[cityName,setCityName]=useState(null)
+ let[weatherData,setWeatherData]=useState(null)
+
+  let getlocation=async(city)=>{
+    let res= await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+  
+  }
+getlocation('Delhi')
+ 
+
+  let fetchData = async () => {
+   let res= await axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+   console.log(res )
+  }
+  
+  // fetchData()
+
+  let handleTemperature=()=>{
+  getlocation(cityName)
+  }
+
+// fetchData()
+
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <input onChange={(e)=>setCityName(e.target.value)} 
+      type="text" placeholder='Enter city name' /> <br /><br/>
+     
+      <button onClick={handleTemperature}>Check Weather</button>
+     {weatherData && <h2>Temperature is 33.5°C</h2>}
+    </div>
+  )
+}
+
+export default App
